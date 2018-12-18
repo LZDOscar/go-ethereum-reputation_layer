cedd4fd56d2343eb9c8f3e1b5ae6e66ccb5a06e3
LZDOscar <164386284@qq.com>
2018-12-18 21:44:11 +0800

    add miner register contract

--- contracts/minerbook/contract/minerbook.sol
+++ contracts/minerbook/contract/minerbook.sol
@@ -7,10 +7,26 @@ contract minerbook {
         bytes32 indexed randaoCommitment
     );
 
-    //TODO: address => (register_name, register_ID)
+    event ReputationAdded(
+        bytes32 indexed hashedPubkey,
+        int indexed reputation
+    );
+
+    event ReputationSubed(
+        bytes32 indexed hashedPubkey,
+        int indexed reputation
+    );
+
+    //TODO: address => (register_name, register_ID, enable)
+    //enable：default is true, if the miner is punished because of lowing than REPUTATION_LOWLIMIT
+    // the enable value is false, the address can't register again.
     mapping (bytes32 => bool) public usedHashedPubkey;
 
+    //reputation list: address => reputation value
+    mapping (bytes32 => int) public reputationList;
+
     uint public constant MINER_ADMISSION = 32 ether;
+    int public constant REPUTATION_LOWLIMIT = -250;
     //TODO：we assume the information(register_name, register_ID) that the registers sent are valid
     //TODO: because the contract checks this by database API which government offerd, but the function has not been achieved now.
     //TODO：one register can register miner with one address,so the function must check.
@@ -42,7 +58,56 @@ contract minerbook {
 
         usedHashedPubkey[hashedPubkey] = true;
 
+        //TODO: add reoutation intital
+        reputationList[hashedPubkey] = 0;
+
         emit MinerRegistered(hashedPubkey, _withdrawalAddressbytes32, _randaoCommitment);
+    }
+
+    function addReputation(bytes memory _pubkey, int value) public payable{
+        require(
+            _pubkey.length == 48,
+            "Public key is not 48 bytes"
+        );
+
+        bytes32 hashedPubkey = keccak256(abi.encodePacked(_pubkey));
+
+        //TODO: change condition
+        require(
+            reputationList[hashedPubkey],
+            "Public key is not a miner"
+        );
+
+        reputationList[hashedPubkey] +=  value;
+
+        emit ReputationAdded(hashedPubkey, reputationList[hashedPubkey]);
+    }
+
+    function subReputation(bytes memory _pubkey, int value) public payable{
+        require(
+            _pubkey.length == 48,
+            "Public key is not 48 bytes"
+        );
+
+        bytes32 hashedPubkey = keccak256(abi.encodePacked(_pubkey));
+        //TODO: change condition
+        require(
+            reputationList[hashedPubkey],
+            "Public key is not a miner"
+        );
+
+        reputationList[hashedPubkey] -= value;
+
+        // TODO:check the reputation whether it lower than threshold
+        // if so, the user pubkey is deregister without any miner_admission, and punish miner in reality
+        if(reputationList[hashedPubkey] <= REPUTATION_LOWLIMIT)
+        {
+            usedHashedPubkey[hashedPubkey] = false;
+            // usedHashedPubkey[hashedPubkey].enable = false;
+            // delete reputationList[hashedPubkey]
+        }
+
+        emit ReputationSubed(hashedPubkey, reputationList[hashedPubkey]);
     }
 
 }
